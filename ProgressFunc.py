d06986c9abe4c6342261c964f289e8f4e88cd4ce
Andrey PC <t2uwsmd@gmail.com>
2025-05-31 10:36:56 +0300

    1

--- ProgressFunc.py
+++ ProgressFunc.py
@@ -1,5 +1,12 @@
 class Progress:
     def __init__(self):
         self.x = 0
-    def next(self):
+    def next(self, dx):
         pass
+
+class ProgressLinear(Progress):
+    def __init__(self):
+        super().__init__()
+    def next(self, dx):
+        self.x += dx
+        return self.x
